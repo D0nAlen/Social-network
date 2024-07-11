e19e5dee622b833781ba106c07c3c3d93194f923
Alena <vasilevnalena.2012@gmail.com>
2024-07-11 14:52:32 +0400

    Added useParams(WIP).

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,6 @@ import MyPostsContainer from "./MyPosts/MyPostsContainer.jsx";
 const Profile = (props) => {
   return (
     <div>
-      {/* <ProfileInfo profile={props.props} /> */}
       <ProfileInfo profile={props.profile} />
       <MyPostsContainer />
     </div>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,16 +3,15 @@ import Profile from "./Profile.jsx";
 import axios from "axios";
 import { connect } from "react-redux";
 import { setUserProfileActionCreator } from "../../redux/profileReducer.js";
-// import { withRouter } from "react-router-dom";
-import { useLocation } from "react-router";
+import { useParams } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
-    let userId = this.props.match.params.userId;
-
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then((response) => {
+    let userId = GetParamsUser();
+    // let userId = this.props.match.params.userId;
+    if (!userId) userId = 2;
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId).then((response) => {
       this.props.setUserProfileActionCreator(response.data);
-      const location = useLocation();
     });
   }
 
@@ -21,10 +20,16 @@ class ProfileContainer extends React.Component {
   }
 }
 
+let GetParamsUser = () => {
+  let { userId } = useParams();
+  console.log(userId);
+  return userId;
+};
+
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-// let WithURLDataContainerComponent = withRouter(ProfileContainer);
+export default connect(mapStateToProps, { setUserProfileActionCreator })(ProfileContainer);
 
-export default connect(mapStateToProps, { setUserProfileActionCreator })(WithURLDataContainerComponent);
+// export default connect(mapStateToProps, { setUserProfileActionCreator })(WithURLDataContainerComponent);
